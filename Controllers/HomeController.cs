4a9795ba211eaa2ef7630bef58f136dc5caafcc7
DELL <okayumair@gmail.com>
2022-07-02 12:28:22 +0500

    Form Quiz

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,5 +8,22 @@ namespace VERTECH.Controllers
         {
             return View();
         }
+
+        public IActionResult ContactUs()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Send(string fname, string lname, string email, int phnum, string genderr, string dep)
+        {
+            ViewBag.Message1 = "Your First Name is: " + fname;
+            ViewBag.Message2 = "Your Last Name is: " + lname;
+            ViewBag.Message3 = "Your Email is: " + email;
+            ViewBag.Message4 = "Your Phone Number is: " + phnum;
+            ViewBag.Message5 = "Gender: " + genderr;
+            ViewBag.Message6 = "Your Dep is: " + dep;
+            return View();
+        }
     }
 }
